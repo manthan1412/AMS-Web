576cd8d86a9e02b91af8889020ff08bc14e7a6ab
Manthan Vaghasiya <mvaghasiya1412@gmail.com>
2016-04-05 17:14:12 +0530

    Initial commit

---
+++ JS/ams.js
@@ -0,0 +1,27 @@
+$(function () {
+
+	$("#demo").append(" test");
+	$("#demo").append(" test");
+
+	$('#submit').click(function OnButtonCLick() {
+		var data = $('#name').val();
+		$('#demo').append(data);
+		// $.toJSON(data);
+		// var data  = jQuery.parseJSON('{ "name" : "dat" }');
+		$('#demo').append("  " + data);
+		$("#demo").append(" test");
+		// $.getJSON("http://localhost:5000/test",data,function (result) {
+		// 	$.each(result, function (i, field) {
+		// 		$("form").append(field + " ");
+		// 	});
+		// });
+		// $.ajax({
+		// 	type: 'POST',
+		// 	url: 'http://localhost:5000/test',
+		// 	data: { get_param: 'value' }, 
+		// 	name: data,
+		// 	dataType: 'json'
+		// });
+		$.post("http://localhost:5000/test", { "name" : data });
+	});
+});
